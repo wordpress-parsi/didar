680b2e8c348791aa2ccfc6358dca04ecb0425a3f
Morteza Geransayeh <man4toman@gmail.com>
2025-04-27 09:30:55 +0330

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -8,24 +8,6 @@ function did_get_sku($pid) {
 	$sku = "SKU_$pid";
 	update_post_meta($pid, "_sku",$sku);
 	return $sku;
-	/*$product = wc_get_product($variation_id);
-
-    if ($product && $product->is_type('variation')) {
-        $parent_id = $product->get_parent_id();
-        $parent_product = wc_get_product($parent_id);
-
-        if ($parent_product) {
-            $variations = $parent_product->get_available_variations();
-
-            foreach ($variations as $variation) {
-                if ($variation['variation_id'] === $variation_id) {
-                    return $variation['sku'];
-                }
-            }
-        }
-    }
-
-    return '';*/
 }
 
 function add_errorlog($order_id,$path,$params,$output){
@@ -34,9 +16,7 @@ function add_errorlog($order_id,$path,$params,$output){
 	$output = empty($output)?[]:$output;
 	$params = json_encode($params,JSON_UNESCAPED_UNICODE);
 	$output = json_encode($output,JSON_UNESCAPED_UNICODE);
-	/*$out = $wpdb->get_var("select id from {$wpdb->prefix}didar_error where path='$path' and params='$params' and date>(NOW() - INTERVAL 5 MINUTE)");
-	if(!empty($out))
-		return $out;*/
+
 	return $wpdb->insert("{$wpdb->prefix}didar_error",[
 		'date'    =>date('Y-m-d H:i:s'),
 		'order_id'=>$order_id,
